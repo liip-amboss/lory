99f5bce3d459b4c5d323412ae450a60ad078647d
Christof Leuenberger <christof.leuenberger@liip.ch>
2017-09-04 13:19:21 +0200

    use a default slot to allow overriding prev/next svgs

--- src/Prev.vue
+++ src/Prev.vue
@@ -1,6 +1,8 @@
 <template>
   <span class="js_prev prev">
-    <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 501.5 501.5"><g><path :fill="color" d="M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z"></path></g></svg>
+    <slot>
+      <svg xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 501.5 501.5"><g><path :fill="color" d="M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z"></path></g></svg>
+    </slot>
   </span>
 </template>
 
